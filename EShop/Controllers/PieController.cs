0bceddbb22ec5b9716d81fad76818e0046e73e3c
Alex <alex.kachmar20@gmail.com>
2020-01-29 23:25:36 +0200

    Small change (testing visual studio git)

--- EShop/Controllers/PieController.cs
+++ EShop/Controllers/PieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EShop.Models;
+using EShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -23,8 +24,10 @@ namespace EShop.Controllers
 
         public ViewResult List()
         {
-            ViewBag.CurrentCategory = "Lemon cheescake";
-            return View(_pieRepository.AllPies);
+            PieViewModel pieViewModel = new PieViewModel();
+            pieViewModel.Pies = _pieRepository.AllPies;
+            pieViewModel.CurrentCategory = "Fishy squishy";
+            return View(pieViewModel);
         }
     }
 }
