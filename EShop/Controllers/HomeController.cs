e2129b0efcde246445f5ce88886d9520ae865fa2
Alex <alex.kachmar20@gmail.com>
2020-01-30 13:58:36 +0200

    Adding partial views

---
+++ EShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EShop.Models;
+using EShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
