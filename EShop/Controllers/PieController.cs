785cfbbbf89cd5dc01dae05561bf2efe6f1f7f74
Alex <alex.kachmar20@gmail.com>
2020-01-31 22:21:57 +0200

    Adding View Components

--- EShop/Controllers/PieController.cs
+++ EShop/Controllers/PieController.cs
@@ -22,14 +22,32 @@ namespace EShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
-        {
-            PieViewModel pieViewModel = new PieViewModel();
-            pieViewModel.Pies = _pieRepository.AllPies;
-            pieViewModel.CurrentCategory = "Fishy squishy";
-            return View(pieViewModel);
-        }
+        //public ViewResult List()
+        //{
+        //    PieViewModel pieViewModel = new PieViewModel();
+        //    pieViewModel.Pies = _pieRepository.AllPies;
+        //    pieViewModel.CurrentCategory = "Fishy squishy";
+        //    return View(pieViewModel);
+        //}
 
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if(string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category).OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieViewModel { Pies = pies, CurrentCategory = currentCategory });
+        }
         public IActionResult Details (int id)
         {
             var pie = _pieRepository.GetPieById(id);
